d6e4f67ab2cc6d2f37eaef5e28fca0cc8d8e89fb
Maurizio <99482352+wbtcpip2@users.noreply.github.com>
2026-03-26 22:54:33 +0100

    Update ffmpeg.cmake

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -107,6 +107,7 @@ ExternalProject_Add(ffmpeg
         --enable-amf
         --enable-openal
         --enable-opengl
+        --disable-x86asm
         --disable-filter=atadenoise
         --disable-doc
         --disable-ffplay
