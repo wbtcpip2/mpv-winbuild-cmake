224503958df2a710f0d5ad1253758e51a18fb30c
Maurizio <99482352+wbtcpip2@users.noreply.github.com>
2026-04-28 00:44:36 +0200

    Update openssl.cmake

--- packages/openssl.cmake
+++ packages/openssl.cmake
@@ -3,7 +3,6 @@ ExternalProject_Add(openssl
         zlib
         zstd
         brotli
-
     GIT_REPOSITORY https://github.com/openssl/openssl.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
@@ -11,65 +10,55 @@ ExternalProject_Add(openssl
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/openssl-*.patch
-
     CONFIGURE_COMMAND ${EXEC} mkdir -p apps/include
     COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/Configure
         --cross-compile-prefix=${TARGET_ARCH}-
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=lib
         --release
-
         no-autoload-config
         ${openssl_target}
         ${openssl_ec_opt}
-
-        # Sicuri
-        no-ssl3
         no-ssl3-method
+        enable-brotli
         no-whirlpool
+        no-filenames
         no-camellia
+        enable-zstd
+        no-capieng
+        no-shared
+        no-rmd160
+        no-module
+        no-legacy
+        no-tests
+        threads
+        no-docs
+        no-apps
+        no-ocsp
+        no-ssl3
+        no-cmac
+        no-mdc2
         no-idea
         no-cast
         no-seed
         no-aria
-        no-md4
-        no-mdc2
-        no-bf
+        no-err
+        no-dso
+        no-dsa
+        no-srp
         no-rc2
         no-rc4
-        no-srp
         no-sm2
         no-sm3
         no-sm4
-        no-capieng
-
-        # Compressioni
-        enable-brotli
-        enable-zstd
+        no-md4
+        no-cms
+        no-cmp
+        no-dh
+        no-bf
         zlib
-
-        # Build pulita
-        no-tests
-        no-docs
-        no-apps
-        threads
-
-        # ⚠️ RIMOSSI i flag che rompono la build:
-        # no-dh
-        # no-dsa
-        # no-err
-        # no-dso
-        # no-module
-        # no-legacy
-        # no-ocsp
-        # no-cms
-        # no-cmp
-        # no-filenames
-        # no-rmd160
-
     BUILD_COMMAND ${MAKE} build_sw
     INSTALL_COMMAND ${MAKE} install_sw
-
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
